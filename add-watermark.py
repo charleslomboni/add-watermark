2ea516e816924d59919ef3edcad07fe12a397926
charleslomboni <charleslomboni@gmail.com>
2020-03-09 20:40:57 -0300

    Refactoring and add option to windows

--- add-watermark.py
+++ add-watermark.py
@@ -4,8 +4,10 @@
 #
 # Author:      Charles Lomboni
 #
-# Created:     27/08/2018
-# Copyright:   (c) Charles Lomboni 2018
+# Created:     27/08/2019
+# Updated:     09/03/2020
+#
+# Copyright:   (c) Charles Lomboni 2019
 # Licence:     MIT
 #-------------------------------------------------------------------------------
 
@@ -13,13 +15,17 @@ import PyPDF2
 import argparse
 import os
 
+folderToSave = "Done"
+splitSlash = ""
+
 def getargs():
     parser = argparse.ArgumentParser()
 
     parser.usage
     parser.add_argument("-w", "--watermarkFile", help="Watermark.pdf to merge")
     parser.add_argument("-d", "--directory", help="Path with all pdf files")
-    
+    parser.add_argument("-s", "--opSystem", help="Operation System by default is linux, if you are runing on windows use .: -s win")
+
 
     return parser.parse_args()
 
@@ -35,14 +41,14 @@ def mergePages(pdfReader, watermarkFile, pdfWriter, wFile, number):
     return pdfWriter
 
 
-def insertWatermark(watermarkFile, originalFile, oneFile=False):
-    
+def insertWatermark(watermarkFile, originalFile, oneFile=False, opSystem="lin"):
+
     originalFile = open(originalFile,'rb')
     pdfReader = PyPDF2.PdfFileReader(originalFile)
     wFile = open(watermarkFile, 'rb')
     pdfWriter = PyPDF2.PdfFileWriter()
     maxLen = pdfReader.numPages
-    
+
     count = 0
     while (count < maxLen):
         pdfWriter = mergePages(pdfReader, watermarkFile, pdfWriter, wFile, count)
@@ -51,14 +57,17 @@ def insertWatermark(watermarkFile, originalFile, oneFile=False):
     if (oneFile == True):
         outputName = originalFile.name
     else:
-        outputName = str(originalFile.name).split('/')[1]
+        splitLen = 0
+        splitLen = len(str(originalFile.name).split(splitSlash))
+        outputName = str(originalFile.name).split(splitSlash)[splitLen-1]
 
-    pathToSave = str(os.getcwd() + '/done')
+
+    pathToSave = str(os.path.dirname(originalFile.name) + splitSlash + folderToSave)
     if (os.path.exists(pathToSave) == False):
         os.mkdir(pathToSave)
 
 
-    resultPdfFile = open(pathToSave+'/'+outputName, 'wb')
+    resultPdfFile = open(pathToSave + splitSlash + outputName, 'wb')
     pdfWriter.write(resultPdfFile)
     originalFile.close()
     wFile.close()
@@ -80,15 +89,19 @@ def main():
 
     watermark = args.watermarkFile
     directory = args.directory
+    opSystem  = args.opSystem
+
+    global splitSlash
+    splitSlash = ("/", "\\")["win" == opSystem]
 
     if (str(directory).find('.pdf') == -1):
         files = getAllPdfFiles(directory)
         for f in files:
-            insertWatermark(watermark, f)
-            print('=> Saved file: [ done/' + f.split('/')[1] + ' ]')
+            insertWatermark(watermark, f, opSystem=opSystem)
+            print('=> Saved file: [ ' + folderToSave + splitSlash + f.split(splitSlash)[len(f.split(splitSlash))-1] + ' ]')
     else:
         insertWatermark(watermark, directory, oneFile=True)
-        print('=> Saved file: [ done/' + directory + ' ]')
+        print('=> Saved file: [ ' + folderToSave + splitSlash + directory + ' ]')
 
 
 
